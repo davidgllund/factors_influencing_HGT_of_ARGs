0f35ac088133be00373585e0a0f8c3f3f173b1c8
davidgllund <davidgllund@gmail.com>
2024-02-20 09:36:41 +0100

    upload files

---
+++ scripts/separate_genome_groups.py
@@ -0,0 +1,74 @@
+from sys import argv
+import argparse
+import subprocess
+
+import pandas as pd
+
+def parse_args(argv):
+    desc = ''
+    copyright = 'Copyright (c) David Lund 2023.'
+    parser = argparse.ArgumentParser(description=desc+'. '+copyright)
+    parser.add_argument('--input', '-i', required=True,
+                        help='Table prodiced by "indentify_horizontal_transfers.R".')
+    parser.add_argument('--output', '-o', required=True,
+                        help='Name of output directory.')
+    
+    arguments = parser.parse_args()
+
+    return arguments
+
+def split_ids(table, column):    
+    df = pd.DataFrame({'id': table.loc[1,column].split(';')})
+    df[['id', 'species']] = df['id'].str.split('-', expand=True)
+    df = df.drop('species', axis=1)
+
+    return df
+
+def extract_sequences(header_subset, fasta_subset, header_complete, fasta_complete):
+    with open(header_subset, 'r') as file:
+        h1 = file.readlines()
+
+    with open(header_complete, 'r') as file:
+        h2 = file.readlines()
+
+    with open(fasta_complete) as original_fasta, open(fasta_subset, 'w') as corrected_fasta:
+        records = SeqIO.parse(original_fasta, 'fasta')
+        i = 0
+        for record in records:
+            if h2[i] not in h1:
+                i += 1
+                continue
+            else:
+                SeqIO.write(record, corrected_fasta, 'fasta')
+                i += 1
+                continue
+
+def main():    
+    arguments = parse_args(argv)
+    table = pd.read_csv(arguments.input, sep="\t")
+        
+    subprocess.run('mkdir %s' %(arguments.output), shell=True)
+        
+    for i in range(len(table.iloc[:,0])):
+        subdir1 = '-'.join([table.iloc[i,0], table.iloc[i,1], 'grp1'])
+        subprocess.run('mkdir %s/%s' %(arguments.output, subdir1), shell=True)
+    
+        ids1 = split_ids(table, "Species1")
+        ids1.to_csv('%s/%s/accession_ids.txt' %(arguments.output, subdir1), index=False, header=False)
+
+        subprocess.run('while read line; do grep "$line" example_data/%s/fasta_headers.txt >> %s/%s/header_subset.txt; done<%s/%s/accession_ids.txt' %(table.iloc[i,1], arguments.output, subdir1, arguments.output, subdir1), shell=True)
+
+        extract_sequences('%s/%s/header_subset.txt' %(arguments.output, subdir1), '%s/%s/nucleotides.fna' %(arguments.output, subdir1), 'example_data/%s/fasta_headers.txt' %(table.iloc[i,1]), 'example_data/%s/predicted-orfs.fasta' %(table.iloc[i,1]))
+
+        subdir2 = '-'.join([table.iloc[i,0], table.iloc[i,1], 'grp2'])
+        subprocess.run('mkdir %s/%s' %(arguments.output, subdir2), shell=True)
+    
+        ids2 = split_ids(table, "Species2")
+        ids2.to_csv('%s/%s/accession_ids.txt' %(arguments.output, subdir2), index=False, header=False)
+
+        subprocess.run('while read line; do grep "$line" example_data/%s/fasta_headers.txt >> %s/%s/header_subset.txt; done<%s/%s/accession_ids.txt' %(table.iloc[i,1], arguments.output, subdir2, arguments.output, subdir2), shell=True)
+
+        extract_sequences('%s/%s/header_subset.txt' %(arguments.output, subdir2), '%s/%s/nucleotides.fna' %(arguments.output, subdir2), 'example_data/%s/fasta_headers.txt' %(table.iloc[i,1]), 'example_data/%s/predicted-orfs.fasta' %(table.iloc[i,1]))
+
+if __name__ == '__main__':
+    main()
